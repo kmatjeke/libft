a459814596f260f94bf5c321b057ff9416044a73
Koketso MATJEKE <kmatjeke@c5r6s7.wethinkcode.co.za>
2019-05-20 15:06:31 +0200

    library and make file update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kmatjeke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:39:03 by kmatjeke          #+#    #+#             */
-/*   Updated: 2019/05/20 14:26:18 by kmatjeke         ###   ########.fr       */
+/*   Updated: 2019/05/20 14:58:18 by kmatjeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ char	*ft_strdup(char *str);
 char	*ft_strcpy(char *dest, char *str);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t len);
 int		ft_strlen(char *str);
 void	ft_putchar(char c);
 void	ft_putstr(char const *str);
