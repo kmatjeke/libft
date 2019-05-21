cfc1b859e5cc9f78de44b26e5df405e84971820f
Koketso MATJEKE <kmatjeke@c5r6s7.wethinkcode.co.za>
2019-05-21 15:32:05 +0200

    ft_strcmp and update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # libft
 first project of WTC containing recreated library
 strlcat skipped, to be done later
+strnstr skipped, to be done later
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kmatjeke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:39:03 by kmatjeke          #+#    #+#             */
-/*   Updated: 2019/05/21 14:45:37 by kmatjeke         ###   ########.fr       */
+/*   Updated: 2019/05/21 15:24:47 by kmatjeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ char	*ft_strncat(char *s1, const char *s2, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strlen(char *str);
 void	ft_putchar(char c);
 void	ft_putstr(char const *str);
