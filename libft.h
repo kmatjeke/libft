463817068c7a833ad2e46d236ca784522b0cad6e
Koketso MATJEKE <kmatjeke@c5r6s7.wethinkcode.co.za>
2019-05-22 14:45:42 +0200

    ft_isalpha and updates

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kmatjeke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:39:03 by kmatjeke          #+#    #+#             */
-/*   Updated: 2019/05/22 12:48:27 by kmatjeke         ###   ########.fr       */
+/*   Updated: 2019/05/22 14:26:35 by kmatjeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strlen(char *str);
 int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
 void	ft_putchar(char c);
 void	ft_putstr(char const *str);
 #endif
--- main.c
+++ main.c
@@ -6,26 +6,52 @@
 /*   By: kmatjeke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:59:55 by kmatjeke          #+#    #+#             */
-/*   Updated: 2019/05/22 12:56:39 by kmatjeke         ###   ########.fr       */
+/*   Updated: 2019/05/22 14:42:56 by kmatjeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <ctype.h>
 
 int main () {
-   int val;
-   char str[20];
+   int var1 = 'd';
+   int var2 = 'Y';
+   int var3 = '\t';
+   int var4 = ' ';
+    
+   if (ft_isalpha(var1))
+   {
+      printf("var1 = |%c| is an alphabet\n", var1 );
+   }
+   else
+   {
+      printf("var1 = |%c| is not an alphabet\n", var1 );
+   }
+   if (ft_isalpha(var2) ) {
+      printf("var2 = |%c| is an alphabet\n", var2 );
+   }
+   else
+   {
+      printf("var2 = |%c| is not an alphabet\n", var2 );
+   }
+   if (ft_isalpha(var3))
+   {
+      printf("var3 = |%c| is an alphabet\n", var3 );
+   }
+   else
+   {
+      printf("var3 = |%c| is not an alphabet\n", var3 );
+   }
    
-   strcpy(str, "567");
-   val = ft_atoi(str);
-   printf("String value = %s, Int value = %d\n", str, val);
-
-   strcpy(str, "4546");
-   val = ft_atoi(str);
-   printf("String value = %s, Int value = %d\n", str, val);
-
+   if (ft_isalpha(var4))
+   {
+      printf("var4 = |%c| is an alphabet\n", var4 );
+   }
+   else
+   {
+      printf("var4 = |%c| is not an alphabet\n", var4 );
+   }
    return(0);
 }
