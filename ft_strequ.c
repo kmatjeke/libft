05522b7ca0d7656f9df8a07bc8720c2cbbdb03a9
Koketso MATJEKE <kmatjeke@c4r7s9.wethinkcode.co.za>
2019-06-18 16:40:07 +0200

    updates

--- ft_strequ.c
+++ ft_strequ.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmatjeke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/29 14:47:00 by kmatjeke          #+#    #+#             */
-/*   Updated: 2019/05/29 15:25:43 by kmatjeke         ###   ########.fr       */
+/*   Created: 2019/06/18 15:44:40 by kmatjeke          #+#    #+#             */
+/*   Updated: 2019/06/18 15:49:51 by kmatjeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
+	int	i;
+	int	ret;
+
 	if (s1 && s2)
 	{
-		if (s1 == s2)
+		i = 0;
+		while ((s1[i] != '\0') && (s2[i] != '\0') && (s1[i] == s2[i]))
+			i++;
+		ret = (unsigned char)s1[i] - (unsigned char)s2[i];
+		if (ret == 0)
 			return (1);
 		else
 			return (0);
